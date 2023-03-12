dc7a18cb4bccf039d4ea0b7f8c0edb61c7f95960
gigiftttgi <76035585+gigiftttgi@users.noreply.github.com>
2023-03-12 23:17:06 +0700

    all done

--- App/server/index.js
+++ App/server/index.js
@@ -30,6 +30,8 @@ app.get("/act", function (req, res, next) {
 });
 
 app.post("/join", function (req, res) {
+  const id = req.body.id;
+  const form = req.body.form;
   const name = req.body.name;
   const age = req.body.age;
   const stID = req.body.stID;
@@ -37,21 +39,195 @@ app.post("/join", function (req, res) {
   const faculty = req.body.faculty;
   const year = req.body.year;
   const commu = req.body.commu;
-  const com = req.body.com;
+  var com = "No";
+  if (req.body.com == "Have") {
+    com = "Yes";
+  }
   const phone = req.body.phone;
   const email = req.body.email;
+  const sql1 = "SELECT * From club WHERE ID = " + mysql.escape(id);
+  const sql2 = "SELECT * From activies WHERE ID = " + mysql.escape(id);
+  //
+  var pass = 0;
+  var mem = "";
 
-  connection.query(
-    "INSERT INTO student (Name, Age, StudentID, Gender, Faculty, Year, CommunicationSkill, OwnComputer, Phone, Email) VALUES (?,?,?,?,?,?,?,?,?,?)",
-    [name, age, stID, gender, faculty, year, commu, com, phone, email],
-    (error, result) => {
-      if (error) {
-        console.log(error);
+  if (form == "Clubs") {
+    connection.query(sql1, function (error, results) {
+      if (error) throw error;
+      Object.keys(results).forEach(function (key) {
+        var row = results[key];
+        let y = [];
+        let fac = [];
+        let c = [];
+        const gen = row.Gender.split(",");
+        if (row.Year == "All") {
+          y = [
+            "Master Student",
+            "Graduate Student",
+            "Super senior",
+            "Senior",
+            "Junior",
+            "Junior",
+            "Sophomore",
+            "Freshman",
+          ];
+        } else {
+          y = row.Year.split(",");
+        }
+
+        if (row.Faculty == "All") {
+          fac = [
+            "Faculty of Architecture",
+            "Faculty of Engineering",
+            "Faculty of Science",
+            "Faculty of Education",
+            "Faculty of Education",
+            "Faculty of Agriculture",
+            "Faculty of Veterinary Science",
+            "Faculty of Nursing",
+            "Faculty of Dentistry",
+          ];
+        } else {
+          fac = row.Faculty.split(",");
+        }
+
+        if (row.Computer == "No") {
+          c = ["Yes", "No"];
+        }
+
+        if (
+          age >= row.Age &&
+          c.includes(com) &&
+          commu >= row.CommunicationSkill &&
+          gen.includes(gender) &&
+          fac.includes(faculty) &&
+          y.includes(year)
+        ) {
+          mem = row.Member;
+        } else {
+          pass = 5;
+        }
+      });
+      //   pass = 6;
+      if (pass == 0) {
+        if (mem == "") {
+          mem = name;
+        } else {
+          mem = mem + "," + name;
+        }
+
+        const sql3 =
+          "UPDATE club SET Member = " +
+          mysql.escape(mem) +
+          "WHERE ID = " +
+          mysql.escape(id);
+        connection.query(sql3, function (error, results) {
+          if (error) throw error;
+        });
+        connection.query(
+          "INSERT INTO student (Name, Age, StudentID, Gender, Faculty, Year, CommunicationSkill, OwnComputer, Phone, Email) VALUES (?,?,?,?,?,?,?,?,?,?)",
+          [name, age, stID, gender, faculty, year, commu, com, phone, email],
+          (error, result) => {
+            if (error) {
+              console.log(error);
+            }
+          }
+        );
+        res.json("Complete");
       } else {
-        res.send("Value add");
+        res.json("Reject");
       }
-    }
-  );
+    });
+  } else {
+    connection.query(sql2, function (error, results) {
+      if (error) throw error;
+      console.log("HI");
+      Object.keys(results).forEach(function (key) {
+        var row = results[key];
+        let y = [];
+        let fac = [];
+        const gen = row.Gender.split(",");
+        if (row.Year == "All") {
+          y = [
+            "Master Student",
+            "Graduate Student",
+            "Super senior",
+            "Senior",
+            "Junior",
+            "Junior",
+            "Sophomore",
+            "Freshman",
+          ];
+        } else {
+          y = row.Year.split(",");
+        }
+
+        if (row.Faculty == "All") {
+          fac = [
+            "Faculty of Architecture",
+            "Faculty of Engineering",
+            "Faculty of Science",
+            "Faculty of Education",
+            "Faculty of Education",
+            "Faculty of Agriculture",
+            "Faculty of Veterinary Science",
+            "Faculty of Nursing",
+            "Faculty of Dentistry",
+          ];
+        } else {
+          fac = row.Faculty.split(",");
+        }
+        if (row.Computer == "No") {
+          c = ["Yes", "No"];
+        }
+        if (
+          age >= row.Age &&
+          c.includes(com) &&
+          commu >= row.CommunicationSkill &&
+          gen.includes(gender) &&
+          fac.includes(faculty) &&
+          y.includes(year)
+        ) {
+          mem = row.Member;
+        } else {
+          pass = 5;
+          console.log(age >= row.Age);
+          console.log(c.includes(com));
+          console.log(commu >= row.CommunicationSkill);
+          console.log(gen.includes(gender));
+          console.log(fac.includes(faculty));
+          console.log(y.includes(year));
+        }
+      });
+      if (pass == 0) {
+        if (mem == "") {
+          mem = name;
+        } else {
+          mem = mem + "," + name;
+        }
+        const sql3 =
+          "UPDATE activies SET Member = " +
+          mysql.escape(mem) +
+          "WHERE ID = " +
+          mysql.escape(id);
+        connection.query(sql3, function (error, results) {
+          if (error) throw error;
+        });
+        connection.query(
+          "INSERT INTO student (Name, Age, StudentID, Gender, Faculty, Year, CommunicationSkill, OwnComputer, Phone, Email) VALUES (?,?,?,?,?,?,?,?,?,?)",
+          [name, age, stID, gender, faculty, year, commu, com, phone, email],
+          (error, result) => {
+            if (error) {
+              console.log(error);
+            }
+          }
+        );
+        res.json("Complete");
+      } else {
+        res.json("Reject");
+      }
+    });
+  }
 });
 
 app.post("/detail", function (req, res) {
